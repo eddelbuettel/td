9cdc85242bc13eda6c5510e15a3dc6432b4cdf61
Dirk Eddelbuettel <edd@debian.org>
2021-02-07 09:37:06 -0600

    minimal testing (and a key for testing would be good for CI)

--- inst/tinytest/test_td.R
+++ inst/tinytest/test_td.R
@@ -1,4 +1,17 @@
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+suppressMessages(library(td))
 
+## there is not a lot we can without an API key
+expect_error(time_series(sym="SPY", api=""))
+
+## if we have an API key AND tests are opted-into, run basic tests
+## we use an opt-in to not run down the query allotment
+if ((Sys.getenv("RunTDTests","") == "yes")
+    && td:::.get_apikey() != ""
+    && requireNamespace("xts", quietly=TRUE)) {
+
+    spy <- time_series(sym="SPY", interval="1min", outputsize=100, as="xts")
+
+    expect_true(inherits(spy, "xts"))
+    expect_equal(nrow(spy), 100)
+}
